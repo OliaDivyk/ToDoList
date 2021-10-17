786281a2943b99b7607c6fe3d25a470919300b8c
OliaDivyk <olia.divyk@gmail.com>
2021-10-18 01:36:59 +0300

    upload js

--- js/main.js
+++ js/main.js
@@ -55,3 +55,66 @@ function deleteTask(taskId) {
         }
     });
 }
+
+$('#register').on('click', function()
+{
+    if($('#name').val() && $('#mail').val() && $('#password').val() && $('#confirmationpassword').val()) {
+        SendDataRegister();
+    }
+}); 
+
+function SendDataRegister() {
+    var name = $('#name').val();
+    var mail = $('#mail').val();
+    var password = $('#password').val();
+    var confirmationpassword = $('#confirmationpassword').val();
+
+    $('#name').val('');
+    $('#mail').val('');
+    $('#password').val('');
+    $('#confirmationpassword').val('');
+    $.ajax({
+        url: "./RegisterController.php", 
+        method: "POST", 
+        data: {name, mail, password, confirmationpassword},
+            success: function(data) {	
+                window.location.href = "/SignIn.php";
+          }
+      });
+}
+
+$('#signIn').on('click', function()
+{
+    if($('#mail').val() && $('#password').val()) {
+        SendDataSignIn();
+    }
+}); 
+
+function SendDataSignIn() {
+    var mail = $('#mail').val();
+    var password = $('#password').val();
+
+    $('#mail').val('');
+    $('#password').val('');
+
+    $.ajax({
+        url: "./SignInController.php", 
+        method: "POST", 
+        data: {mail, password},
+            success: function(data) {	
+                window.location.href = "/dashboard.php";
+          }
+      });
+      
+}
+
+$('#signout').on('click', function() {
+    $.post("/SignOutController.php", function( data ) {
+        window.location.href = "/SignIn.php";
+    });
+});
+
+$('#btnStarted').on('click', function() {
+    window.location.href = "/SignIn.php";
+});
+
